a0896634fd15f3c2189e7b4c83ddc00205ef2293
azambembem <azamjan980713@gmail.com>
2024-11-12 23:53:27 +0900

    first commit

---
+++ Monotic_Array.js
@@ -0,0 +1,18 @@
+var isMonotonic = function (nums) {
+  let increasing = true;
+  let decreasing = true;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      decreasing = false;
+    } else if (nums[i] < nums[i - 1]) {
+      increasing = false;
+    }
+  }
+
+  return increasing || decreasing;
+};
+
+console.log(isMonotonic((nums = [1, 2, 2, 3])));
+console.log(isMonotonic((nums = [6, 5, 4, 4])));
+console.log(isMonotonic((nums = [1, 3, 2])));

392d2f9c794a4f18d951b4f5d3be277e93dc6153
azambembem <azamjan980713@gmail.com>
2024-11-18 23:57:30 +0900

    second commit

--- Binary_Gap.js
+++ Binary_Gap.js
@@ -1,21 +1,10 @@
-var binaryGap = function (N) {
-  let binary = N.toString(2);
-  let longestGap = 0;
-  let currentGap = 0;
-  let i = 0;
-
-  while (i < binary.length && binary[i] !== "1") {
-    i++;
-  }
-
-  for (i = i + 1; i < binary.length; i++) {
-    if (binary[i] === "1") {
-      longestGap = Math.max(longestGap, currentGap);
-      currentGap = 0;
-    } else {
-      currentGap++;
+function binaryGap(n) {
+  let ans = 0;
+  for (let d = -32; n > 0; n = Math.floor(n / 2), d++) {
+    if (n % 2 === 1) {
+      ans = Math.max(ans, d);
+      d = 0;
     }
   }
-
-  return longestGap;
-};
+  return ans;
+}

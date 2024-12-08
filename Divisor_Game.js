5ce695492d9fc6d572d3996d0e8f939eb875178a
azambembem <azamjan980713@gmail.com>
2024-12-08 23:20:18 +0900

    first commit

---
+++ Divisor_Game.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function (n) {
+  return n % 2 === 0;
+};
+
+console.log(divisorGame(2));
+console.log(divisorGame(3));
+console.log(divisorGame(10));
